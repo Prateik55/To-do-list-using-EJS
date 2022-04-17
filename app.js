ab105b68554d65b56f5ce60d009a4cf8bf5e2b06
Prateik <itspsr5@gmail.com>
2022-04-17 19:07:21 +0530

    adding work to do list using template

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser= require ("body-parser");
 
 const app = express();
-let items=[];
+const items=[];
+const workItems=[];
 
 
 app.set('view engine', 'ejs');
@@ -22,14 +23,30 @@ app.get('/', function(req, res) {
     let day =today.toLocaleDateString("en-IN",options);
     
     
-    res.render('list', {kindOfDay:day, newListItems:items});
+    res.render('list', {listTitle:day, newListItems:items});
 });
     app.post("/",function (req,res){
-    let item = req.body.newItem;
 
-    items.push(item) ;
-    res.redirect("/")
+    let item = req.body.newItem;
+    
+    if (req.body.list === "Work"){
+        workItems.push(item)
+        res.redirect("/work");
+    }else{
+        items.push(item) ;
+        res.redirect("/");
+    }
+    
  });
+ 
+ app.get("/work", function(req,res){
+     res.render("list",{listTitle:"Work list",newListItems:workItems});
+ })
+//  app.post("/work",function(req,res){
+//      let item=req.body.newItems;
+//      workItems.push(item);
+//      res.redirect("/work");
+//  });
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
